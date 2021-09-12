3481769d9b6c1a95bdbc68c6e2561bc26b6c114a
Marcelo Mesa <marcelomesarod@hotmail.com>
2021-09-12 18:17:08 -0300

    Se eliminaron archivos innecesarios y se agrego el componente NavBar con su hoja de estilo correspondiente

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+    return (
+        <nav className="navBar">
+            <ul>
+                <li><a href="#" className="active">Coffee Shop</a></li>
+                <li><a href="#">Tienda</a></li>
+                <li><a href="#">Nosotros</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Carrito</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
+
